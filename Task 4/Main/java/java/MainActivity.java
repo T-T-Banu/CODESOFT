a36b77763401044fe9a26ed5414f1d63eb68f4bc
T.T Banu <110076253+T-T-Banu@users.noreply.github.com>
2024-04-09 16:41:54 +0530

    Add files via upload

--- Task 4/Main/java/java/MainActivity.java
+++ Task 4/Main/java/java/MainActivity.java
@@ -1,1 +1,29 @@
+package com.example.quizapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to DashboardActivity
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
